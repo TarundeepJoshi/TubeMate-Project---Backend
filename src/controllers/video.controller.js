3e60841df77713b3e996d8cd8f266c081ffb0b8a
Tarundeep Joshi <tarundeepjoshi0002@gmail.com>
2024-04-14 14:24:17 +0530

    complete todo of getAllVideos

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -8,7 +8,49 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+
   //TODO: get all videos based on query, sort, pagination
+
+  const options = {
+    page: parseInt(page),
+    limit: parseInt(limit),
+    sortBy: sortBy
+      ? { [sortBy]: sortType === "desc" ? -1 : 1 }
+      : { createdAt: -1 },
+  };
+
+  const conditions = {};
+  if (query) {
+    conditions.title = { $regex: query, $options: "i" };
+    conditions.description = { $regex: query, $options: "i" };
+  }
+
+  if (userId) {
+    conditions.owner = userId;
+  }
+
+  const videos = await Video.aggregatePaginate(conditions, options);
+
+  for (let video of videos.docs) {
+    const likes = await Like.find({ video: video._id }).populate(
+      "likedBy",
+      "fullName username"
+    );
+
+    video.likes = likes.map((like) => like.likedBy);
+
+    const owner = await User.findById(video.owner).select("fullName username");
+    video.owner = owner;
+  }
+
+  if (!videos) {
+    console.log("error in fetching videos");
+    throw new ApiError(500, "error in fetching videos");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, "videos fetched successfully"));
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
